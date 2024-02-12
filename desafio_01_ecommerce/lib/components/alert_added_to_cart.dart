b7b5f5f1aaa40d9380ff3f2aef4574083c130de3
Leonardo Barbosa <leonardo.pereira@modalgr.com.br>
2024-02-12 15:57:46 -0300

    feat: closing pop when redirectioned to cart's page

--- desafio_01_ecommerce/lib/components/alert_added_to_cart.dart
+++ desafio_01_ecommerce/lib/components/alert_added_to_cart.dart
@@ -2,18 +2,24 @@ import 'package:flutter/material.dart';
 import 'package:flutter_05_ecommerce/pages/cart_page.dart';
 
 class AlertAddedToCart extends StatelessWidget {
-  const AlertAddedToCart({
-    super.key,
-  });
+  const AlertAddedToCart();
 
   @override
   Widget build(BuildContext context) {
     return AlertDialog(
       title: Text('Successfully added!'),
       content: InkWell(
-        child: Text('Check your cart', style: TextStyle(color: Colors.blue),),
-        onTap: () => Navigator.push(context,
-            MaterialPageRoute(builder: (context) => CartPage())),
+        child: Text(
+          'Check your cart',
+          style: TextStyle(color: Colors.blue),
+        ),
+        onTap: () {
+          Navigator.pop(context); // Fechar o AlertDialog
+          Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => CartPage()),
+          );
+        },
       ),
     );
   }
