9f14f411bd52dfef13f2f6115f2a7cb57073a20f
Leonardo Barbosa <leonardo.pereira@modalgr.com.br>
2024-02-15 12:02:08 -0300

    init: inicializando o projeto

---
+++ desafio_02_movies/README.md
@@ -0,0 +1,3 @@
+# desafio_02_movies
+
+A new Flutter project.
---
+++ desafio_02_movies/android/app/src/main/kotlin/com/example/desafio_02_movies/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.desafio_02_movies
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
