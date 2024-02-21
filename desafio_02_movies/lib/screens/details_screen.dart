44a0a183289db30e6e4e6124ef0f5f7ed99e2a79
Leonardo Barbosa <leonardo.pereira@modalgr.com.br>
2024-02-21 11:22:45 -0300

    fix: fixing shared preferences bug

--- desafio_02_movies/lib/screens/details_screen.dart
+++ desafio_02_movies/lib/screens/details_screen.dart
@@ -5,6 +5,7 @@ import 'package:desafio_02_movies/models/movie.dart';
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
+
 class DetailsScreen extends StatefulWidget {
   final Movie movie;
 
@@ -30,26 +31,25 @@ class _DetailsScreenState extends State<DetailsScreen> {
     });
   }
 
-  Future<void> toggleFavoriteStatus() async {
-    SharedPreferences prefs = await SharedPreferences.getInstance();
-    List<String> favoriteIds = prefs.getStringList('favoriteMovies') ?? [];
-    if (isFavorite) {
-      // Remove o filme dos favoritos
-      prefs.remove(widget.movie.id.toString());
-      favoriteIds.remove(widget.movie.id.toString());
-    } else {
-      // Adiciona o filme aos favoritos se ainda não estiver na lista
-      if (!favoriteIds.contains(widget.movie.id.toString())) {
-        favoriteIds.add(widget.movie.id.toString());
-        prefs.setString(
-            widget.movie.id.toString(), jsonEncode(widget.movie.toJson()));
-      }
+Future<void> toggleFavoriteStatus() async {
+  SharedPreferences prefs = await SharedPreferences.getInstance();
+  List<String> favoriteIds = prefs.getStringList('favoriteMovies') ?? [];
+  if (isFavorite) {
+    // Remove o filme dos favoritos
+    prefs.remove(widget.movie.id.toString());
+    favoriteIds.remove(widget.movie.id.toString());
+  } else {
+    // Adiciona o filme aos favoritos se ainda não estiver na lista
+    if (!favoriteIds.contains(widget.movie.id.toString())) {
+      favoriteIds.add(widget.movie.id.toString());
+      prefs.setString(widget.movie.id.toString(), jsonEncode(widget.movie.toJson()));
     }
-    prefs.setStringList('favoriteMovies', favoriteIds);
-    setState(() {
-      isFavorite = !isFavorite;
-    });
   }
+  prefs.setStringList('favoriteMovies', favoriteIds);
+  setState(() {
+    isFavorite = !isFavorite;
+  });
+}
 
   @override
   Widget build(BuildContext context) {
