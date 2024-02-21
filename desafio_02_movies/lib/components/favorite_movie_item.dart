5d51625b0d1b139f7080ff34dbb3e18b31e9e0e7
Leonardo Barbosa <leonardo.pereira@modalgr.com.br>
2024-02-21 12:55:32 -0300

    feat: adding refresh widget to update fav movies

--- desafio_02_movies/lib/components/favorite_movie_item.dart
+++ desafio_02_movies/lib/components/favorite_movie_item.dart
@@ -2,14 +2,15 @@ import 'dart:convert';
 import 'package:desafio_02_movies/colors.dart';
 import 'package:desafio_02_movies/constants.dart';
 import 'package:desafio_02_movies/models/movie.dart';
+import 'package:desafio_02_movies/screens/details_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_rating_bar/flutter_rating_bar.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 class FavoriteMovieItem extends StatefulWidget {
   const FavoriteMovieItem({
-    super.key,
-  });
+    Key? key,
+  }) : super(key: key);
 
   @override
   State<FavoriteMovieItem> createState() => _FavoriteMovieItemState();
@@ -17,6 +18,7 @@ class FavoriteMovieItem extends StatefulWidget {
 
 class _FavoriteMovieItemState extends State<FavoriteMovieItem> {
   List<Movie> favoriteMovies = [];
+  bool _isRefreshing = false;
 
   @override
   void initState() {
@@ -41,6 +43,7 @@ class _FavoriteMovieItemState extends State<FavoriteMovieItem> {
       }
       setState(() {
         favoriteMovies = movies;
+        _isRefreshing = false;
       });
     }
   }
@@ -52,121 +55,161 @@ class _FavoriteMovieItemState extends State<FavoriteMovieItem> {
       favoriteIds.remove(id);
       await prefs.setStringList('favoriteMovies', favoriteIds);
       await prefs.remove(id);
-      getFavoriteMovies();
+      await getFavoriteMovies();
     }
+  }
+
+  Future<void> _handleRefresh() async {
+    setState(() {
+      _isRefreshing = true;
+    });
+    await getFavoriteMovies();
   }
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.only(left: 18, top: 18, right: 18),
-      child: favoriteMovies.isEmpty
-          ? Center(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  const Text(
-                    'Your favorite list is empty.',
-                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 24),
-                  ),
-                  const SizedBox(
-                    height: 22,
-                  ),
-                  InkWell(
-                    child: Container(
-                        decoration: BoxDecoration(
-                            color: Colours.secondaryColor,
-                            borderRadius: BorderRadius.circular(8)),
-                        child: const Padding(
-                          padding: EdgeInsets.symmetric(
-                              horizontal: 18.0, vertical: 8),
-                          child: Text(
-                            'See movies',
-                            style: TextStyle(
-                                fontWeight: FontWeight.bold, fontSize: 22),
-                          ),
-                        )),
-                    onTap: () => Navigator.pop(context),
-                  )
-                ],
-              ),
-            )
-          : ListView.builder(
-              itemCount: favoriteMovies.length,
-              itemBuilder: (context, index) {
-                Movie movie = favoriteMovies[index];
-                return Padding(
-                  padding: const EdgeInsets.only(bottom: 22.0),
-                  child: Row(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Padding(
-                        padding: const EdgeInsets.only(right: 12.0),
-                        child: ClipRRect(
-                          borderRadius: const BorderRadius.only(
-                              topRight: Radius.circular(8),
-                              bottomRight: Radius.circular(8)),
-                          child: Image.network(
-                            '${Constants.imagePath}${movie.posterPath}',
-                            width: 100,
-                          ),
-                        ),
-                      ),
-                      Column(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        crossAxisAlignment: CrossAxisAlignment.start,
+    return Scaffold(
+      body: RefreshIndicator(
+        backgroundColor: Colours.secondaryColor,
+        color: Colors.white,
+        onRefresh: _handleRefresh,
+        child: Padding(
+          padding: const EdgeInsets.only(left: 18, top: 18, right: 18),
+          child: _isRefreshing
+              ? Center(child: CircularProgressIndicator())
+              : favoriteMovies.isEmpty
+                  ? Center(
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
                         children: [
-                          SizedBox(
-                            width: 250,
-                            child: Text(
-                              movie.title,
-                              style: const TextStyle(
-                                  fontWeight: FontWeight.w600, fontSize: 22),
-                            ),
+                          const Text(
+                            'Your favorite list is empty.',
+                            style: TextStyle(
+                                fontWeight: FontWeight.bold, fontSize: 24),
                           ),
-                          Text(
-                            movie.releaseDate,
-                            style: const TextStyle(fontSize: 16),
+                          const SizedBox(
+                            height: 22,
                           ),
-                          Row(
-                            children: [
-                              RatingBar.builder(
-                                initialRating: movie.voteAvarage / 2,
-                                minRating: 0,
-                                direction: Axis.horizontal,
-                                allowHalfRating: true,
-                                itemCount: 5,
-                                itemSize: 24,
-                                ignoreGestures: true,
-                                itemPadding:
-                                    const EdgeInsets.symmetric(horizontal: 2.0),
-                                itemBuilder: (context, index) => const Icon(
-                                  Icons.star,
-                                  color: Colors.amber,
-                                ),
-                                onRatingUpdate: (rating) {
-                                  print(rating);
-                                },
+                          InkWell(
+                            child: Container(
+                              decoration: BoxDecoration(
+                                color: Colours.secondaryColor,
+                                borderRadius: BorderRadius.circular(8),
                               ),
-                              Padding(
-                                padding: const EdgeInsets.only(
-                                    left: 72.0, bottom: 22),
-                                child: IconButton(
-                                  onPressed: () {
-                                    removeFavoriteMovie(movie.id.toString());
-                                  },
-                                  icon: const Icon(Icons.delete),
+                              child: const Padding(
+                                padding: EdgeInsets.symmetric(
+                                    horizontal: 18.0, vertical: 8),
+                                child: Text(
+                                  'See movies',
+                                  style: TextStyle(
+                                      fontWeight: FontWeight.bold,
+                                      fontSize: 22),
                                 ),
-                              )
-                            ],
-                          ),
+                              ),
+                            ),
+                            onTap: () => Navigator.pop(context),
+                          )
                         ],
                       ),
-                    ],
-                  ),
-                );
-              },
-            ),
+                    )
+                  : ListView.builder(
+                      itemCount: favoriteMovies.length,
+                      itemBuilder: (context, index) {
+                        Movie movie = favoriteMovies[index];
+                        return Padding(
+                          padding: const EdgeInsets.only(bottom: 22.0),
+                          child: Row(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Padding(
+                                padding: const EdgeInsets.only(right: 12.0),
+                                child: GestureDetector(
+                                  onTap: () => Navigator.push(
+                                    context,
+                                    MaterialPageRoute(
+                                        builder: (context) =>
+                                            DetailsScreen(movie: movie)),
+                                  ),
+                                  child: ClipRRect(
+                                    borderRadius: const BorderRadius.all(
+                                        Radius.circular(8)),
+                                    child: Image.network(
+                                      '${Constants.imagePath}${movie.posterPath}',
+                                      width: 100,
+                                    ),
+                                  ),
+                                ),
+                              ),
+                              Column(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceBetween,
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  SizedBox(
+                                    width: 250,
+                                    child: Text(
+                                      movie.title,
+                                      style: const TextStyle(
+                                          fontWeight: FontWeight.w600,
+                                          fontSize: 22),
+                                    ),
+                                  ),
+                                  Text(
+                                    movie.releaseDate,
+                                    style: const TextStyle(fontSize: 16),
+                                  ),
+                                  Row(
+                                    children: [
+                                      RatingBar.builder(
+                                        initialRating: movie.voteAvarage / 2,
+                                        minRating: 0,
+                                        direction: Axis.horizontal,
+                                        allowHalfRating: true,
+                                        itemCount: 5,
+                                        itemSize: 24,
+                                        ignoreGestures: true,
+                                        itemPadding: const EdgeInsets.symmetric(
+                                            horizontal: 0.0),
+                                        itemBuilder: (context, index) =>
+                                            const Icon(
+                                          Icons.star,
+                                          color: Colors.amber,
+                                        ),
+                                        onRatingUpdate: (rating) {
+                                          print(rating);
+                                        },
+                                      ),
+                                      Padding(
+                                        padding: const EdgeInsets.only(
+                                            left: 70.0, bottom: 0),
+                                        child: Container(
+                                          decoration: BoxDecoration(
+                                              color: Colours.secondaryColor,
+                                              borderRadius:
+                                                  BorderRadius.circular(50)),
+                                          child: IconButton(
+                                            onPressed: () {
+                                              removeFavoriteMovie(
+                                                  movie.id.toString());
+                                            },
+                                            icon: const Icon(
+                                              Icons.delete,
+                                              color: Colors.white,
+                                            ),
+                                          ),
+                                        ),
+                                      )
+                                    ],
+                                  ),
+                                ],
+                              ),
+                            ],
+                          ),
+                        );
+                      },
+                    ),
+        ),
+      ),
     );
   }
 }
